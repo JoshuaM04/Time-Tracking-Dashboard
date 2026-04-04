14ad3bed25867e7b2167442b916e98d95a77c6f6
Viper <lmi25@txstate.edu>
2026-04-04 13:25:52 -0500

    Adjusted the border radius of the boxes.

--- time-tracking-dashboard/src/App.jsx
+++ time-tracking-dashboard/src/App.jsx
@@ -4,8 +4,8 @@ export default function Dashboard() {
   return (
     <div className="parent-container min-h-screen p-[20px] pt-[60px] bg-gray-900 min-w-screen">
       <main className="min-h-full grid grid-cols-1 grid-rows-7 gap-4 w-[315px]">
-        <div className="box bg-indigo-900 rounded-md p-[20px] text-white">
-          <div className="flex items-center gap-5 mb-[2rem] bg-indigo-500 rounded-md p-[20px] m-[-20px]">
+        <div className="box bg-indigo-900 rounded-xl p-[20px] text-white">
+          <div className="flex items-center gap-5 mb-[2rem] bg-indigo-500 rounded-xl p-[20px] m-[-20px]">
             <img src={portrait} className="w-[70px] border-[5px] rounded-[50%]" />
             <h1>
               <span className="block text-slate-400">Report for</span>
@@ -13,13 +13,13 @@ export default function Dashboard() {
             </h1>
           </div>
           <div className="flex justify-between">
-            <span>Daily</span> 
-            <span>Weekly</span> 
-            <span>Monthly</span>
+            <span className="block text-slate-400">Daily</span> 
+            <span className="font-medium">Weekly</span> 
+            <span className="block text-slate-400">Monthly</span>
           </div>
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-[20px] text-white">
+        <div className="box bg-indigo-900 rounded-xl p-[20px] text-white">
           <h2>Work</h2>
           5hrs  daily
           Previous - 7hrs  daily
@@ -29,7 +29,7 @@ export default function Dashboard() {
           Previous - 128hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-[20px] text-white">
+        <div className="box bg-indigo-900 rounded-xl p-[20px] text-white">
           <h2>Play</h2>
           1hr  daily
           Previous - 2hrs  daily
@@ -39,7 +39,7 @@ export default function Dashboard() {
           Previous - 29hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-[20px] text-white">
+        <div className="box bg-indigo-900 rounded-xl p-[20px] text-white">
           <h2>Study</h2>
           0hrs  daily
           Previous - 1hr  daily
@@ -49,7 +49,7 @@ export default function Dashboard() {
           Previous - 19hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-[20px] text-white">
+        <div className="box bg-indigo-900 rounded-xl p-[20px] text-white">
           <h2>Exercise</h2>
           1hr  daily
           Previous - 1hr  daily
@@ -59,7 +59,7 @@ export default function Dashboard() {
           Previous - 18hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-[20px] text-white">
+        <div className="box bg-indigo-900 rounded-xl p-[20px] text-white">
           <h2>Social</h2>
           1hr  daily
           Previous - 3hrs  daily
@@ -69,7 +69,7 @@ export default function Dashboard() {
           Previous - 23hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-[20px] text-white">
+        <div className="box bg-indigo-900 rounded-xl p-[20px] text-white">
           <h2>Self Care</h2>
           0hrs  daily
           Previous - 1hr  daily
