61fd6ec6c83af560e088588b56e91b132400cac6
Viper <lmi25@txstate.edu>
2026-04-04 12:50:51 -0500

    Adjusted the styling and alignment of the footer.

--- time-tracking-dashboard/src/App.jsx
+++ time-tracking-dashboard/src/App.jsx
@@ -2,7 +2,7 @@ export default function Dashboard() {
   return (
     <div className="parent-container min-h-screen p-[20px] pt-[60px] bg-gray-900 min-w-screen">
       <main className="min-h-full grid grid-cols-1 grid-rows-7 gap-4 w-[315px]">
-        <div className="box bg-indigo-900 rounded-md p-5">
+        <div className="box bg-indigo-900 rounded-md p-[20px]">
           Report for
           Jeremy Robson
           Daily
@@ -10,7 +10,7 @@ export default function Dashboard() {
           Monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-5">
+        <div className="box bg-indigo-900 rounded-md p-[20px]">
           Work
           5hrs  daily
           Previous - 7hrs  daily
@@ -20,7 +20,7 @@ export default function Dashboard() {
           Previous - 128hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-5">
+        <div className="box bg-indigo-900 rounded-md p-[20px]">
           Play
           1hr  daily
           Previous - 2hrs  daily
@@ -30,7 +30,7 @@ export default function Dashboard() {
           Previous - 29hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-5">
+        <div className="box bg-indigo-900 rounded-md p-[20px]">
           Study
           0hrs  daily
           Previous - 1hr  daily
@@ -40,7 +40,7 @@ export default function Dashboard() {
           Previous - 19hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-5">
+        <div className="box bg-indigo-900 rounded-md p-[20px]">
           Exercise
           1hr  daily
           Previous - 1hr  daily
@@ -50,7 +50,7 @@ export default function Dashboard() {
           Previous - 18hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-5">
+        <div className="box bg-indigo-900 rounded-md p-[20px]">
           Social
           1hr  daily
           Previous - 3hrs  daily
@@ -60,7 +60,7 @@ export default function Dashboard() {
           Previous - 23hrs  monthly
         </div>
 
-        <div className="box bg-indigo-900 rounded-md p-5">
+        <div className="box bg-indigo-900 rounded-md p-[20px]">
           Self Care
           0hrs  daily
           Previous - 1hr  daily
@@ -71,9 +71,9 @@ export default function Dashboard() {
         </div>
       </main>
       
-      <footer className="attribution relative bottom-0 text-center -m-5 mt-5 p-5 text-white">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge"><span className="font-medium text-blue-200">Frontend Mentor</span></a>.
-        Coded by <a href="#"><span className="font-medium text-blue-200">Joshua Martinez</span></a>.
+      <footer className="attribution relative bottom-0 text-center -m-5 mt-5 text-white p-[20px] w-[355px]">
+        <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge"><span className="font-medium text-blue-200">Frontend Mentor</span></a></p>
+        <p>Coded by <a href="#"><span className="font-medium text-blue-200">Joshua Martinez</span></a></p>
       </footer>
     </div>
   );
