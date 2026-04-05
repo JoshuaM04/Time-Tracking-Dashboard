9631adbbb7ae1ab7a7f21a8d2c0a81e1c82ae5fe
Viper <lmi25@txstate.edu>
2026-04-04 21:02:34 -0500

    Started adding breakpoints for an xl layout, so that the layout of the webpage changes. Furthermore, adjusted the alignment and sizes of various content.

--- time-tracking-dashboard/src/App.jsx
+++ time-tracking-dashboard/src/App.jsx
@@ -4,23 +4,25 @@ import btnIcon from './assets/images/icon-ellipsis.svg';
 export default function Dashboard() {
   return (
     <div className="parent-container min-h-screen p-[20px] pt-[60px] bg-gray-900 min-w-screen flex flex-col items-center justify-center">
-      <main className="min-h-full grid grid-cols-1 grid-rows-7 gap-4 w-[315px]">
-        <div className="box bg-indigo-900 rounded-xl p-[20px] text-white">
-          <div className="flex items-center gap-5 mb-[2rem] bg-indigo-500 rounded-xl p-[20px] m-[-20px]">
-            <img src={portrait} className="w-[70px] border-[5px] rounded-[50%]" />
+      <main className="min-h-full grid grid-cols-1 grid-rows-7 gap-4 w-[315px] xl:grid-cols-4 xl:grid-rows-2 xl:w-[1000px]">
+
+        <div className="box bg-indigo-900 rounded-xl p-[20px] text-white min-w-[315px] xl:min-w-[100px] xl:row-span-2 xl:min-h-[500px]">
+          <div className="flex items-center gap-5 mb-[2rem] bg-indigo-500 rounded-xl p-[20px] m-[-20px] xl:min-h-[350px] xl:flex-col xl:items-start xl:p-[30px]">
+            <img src={portrait} className="w-[70px] border-[5px] rounded-[50%] xl:w-[100px]" />
             <h1>
               <span className="block text-slate-400">Report for</span>
-              <span className="text-2xl">Jeremy Robson</span>
+              <span className="text-2xl xl:text-4xl">Jeremy Robson</span>
             </h1>
           </div>
-          <div className="flex justify-between">
+
+          <div className="flex justify-between xl:flex-col xl:items-start">
             <button className="block text-slate-400 hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg">Daily</button> 
             <button className="font-medium hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg">Weekly</button> 
             <button className="block text-slate-400 hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg">Monthly</button>
           </div>
         </div>
 
-        <section className="bg-orange-400 rounded-xl flex items-end bg-[url(assets/images/icon-work.svg)] bg-no-repeat bg-position-[top_right_15px]">
+        <section className="bg-orange-400 rounded-xl flex items-end bg-[url(assets/images/icon-work.svg)] bg-no-repeat bg-position-[top_right_15px] xl:min-w-[100px]">
           <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px]">
             <div>
               <h2 className="font-medium">Work</h2>
@@ -42,8 +44,8 @@ export default function Dashboard() {
           </div>
         </section>
 
-        <section className="bg-blue-400 rounded-xl flex items-end bg-[url(assets/images/icon-play.svg)] bg-no-repeat bg-position-[top_right_15px]">
-          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px]">
+        <section className="bg-blue-400 rounded-xl flex items-end bg-[url(assets/images/icon-play.svg)] bg-no-repeat bg-position-[top_right_15px] xl:min-w-[100px]">
+          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px] xl:min-w-[315px]">
             <div>
               <h2 className="font-medium">Play</h2>
               <p className="text-3xl">10hrs</p>
@@ -65,7 +67,7 @@ export default function Dashboard() {
         </section>
 
         <section className="bg-red-400 rounded-xl flex items-end bg-[url(assets/images/icon-study.svg)] bg-no-repeat bg-position-[top_right_15px]">
-          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px]">
+          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px] xl:min-w-[315px]">
             <div>
               <h2 className="font-medium">Study</h2>
               <p className="text-3xl">4hrs</p>
@@ -87,7 +89,7 @@ export default function Dashboard() {
         </section>
 
         <section className="bg-green-400 rounded-xl flex items-end bg-[url(assets/images/icon-exercise.svg)] bg-no-repeat bg-position-[top_right_15px]">
-          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px]">
+          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px] xl:min-w-[315px]">
             <div>
               <h2 className="font-medium">Exercise</h2>
               <p className="text-3xl">4hrs</p>
@@ -109,7 +111,7 @@ export default function Dashboard() {
         </section>
 
         <section className="bg-violet-600 rounded-xl flex items-end bg-[url(assets/images/icon-social.svg)] bg-no-repeat bg-position-[top_right_15px]">
-          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px]">
+          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px] xl:min-w-[315px]">
             <div>
               <h2 className="font-medium">Social</h2>
               <p className="text-3xl">5hrs</p>
@@ -131,7 +133,7 @@ export default function Dashboard() {
         </section>
 
         <section className="bg-yellow-300 rounded-xl flex items-end bg-[url(assets/images/icon-self-care.svg)] bg-no-repeat bg-position-[top_right_15px]">
-          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px]">
+          <div className="flex justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[135px] xl:min-w-[315px]">
             <div>
               <h2 className="font-medium">Self Care</h2>
               <p className="text-3xl">2hrs</p>
@@ -153,7 +155,7 @@ export default function Dashboard() {
         </section>
       </main>
       
-      <footer className="attribution relative bottom-0 text-center -m-5 mt-5 text-white p-[20px] w-[355px]">
+      <footer className="attribution relative bottom-0 text-center -m-5 mt-5 text-white p-[20px] w-[355px] xl:absolute">
         <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge"><span className="font-medium text-blue-200">Frontend Mentor</span></a></p>
         <p>Coded by <a href="#"><span className="font-medium text-blue-200">Joshua Martinez</span></a></p>
       </footer>
