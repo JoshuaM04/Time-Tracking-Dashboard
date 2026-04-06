2df61f40943b3c71f4e46dbb68534343cb6b2f0c
Viper <lmi25@txstate.edu>
2026-04-06 14:07:10 -0500

    Utilized a .map() callback function to render each box to the page rather than doing it manually 6 times. Furthermore, imported the data from the data.json file to have the data be dynamic rather than hard-coded. Lastly, updated the styles to be dynamic for each element. This reduces the bloat of the program.

--- time-tracking-dashboard/src/App.jsx
+++ time-tracking-dashboard/src/App.jsx
@@ -1,11 +1,17 @@
 import portrait from './assets/images/image-jeremy.png';
 import btnIcon from './assets/images/icon-ellipsis.svg';
+import data from "../data.json";
+import { useState } from 'react';
 
 export default function Dashboard() {
+  const [boxes, setBoxes] = useState(data);
+  const bgColor = ["bg-orange-400", "bg-blue-400", "bg-red-400", "bg-green-400", "bg-violet-600", "bg-yellow-300"]
+  const bgImage =["bg-[url(assets/images/icon-work.svg)]", "bg-[url(assets/images/icon-play.svg)]", "bg-[url(assets/images/icon-study.svg)]", "bg-[url(assets/images/icon-exercise.svg)]", "bg-[url(assets/images/icon-social.svg)]", "bg-[url(assets/images/icon-self-care.svg)]"];
+  let count = -1;
+
   return (
     <div className="parent-container min-h-screen p-[20px] pt-[60px] bg-gray-900 min-w-screen flex flex-col items-center justify-center">
       <main className="min-h-full grid grid-cols-1 grid-rows-7 gap-4 w-[315px] items-center xl:grid-cols-4 xl:grid-rows-2 xl:w-[1000px] xl:gap-8">
-
         <div className="flex flex-col justify-between box bg-indigo-900 rounded-xl p-[20px] text-white min-w-[315px] min-h-[160px] xl:min-w-[100px] xl:row-span-2 xl:min-h-[422px]">
           <div className="flex items-center gap-5 bg-indigo-500 rounded-xl p-[20px] m-[-20px] max-h-[90px] xl:min-h-[280px] xl:flex-col xl:items-start xl:p-[30px]">
             <img src={portrait} className="w-[60px] xl:w-[70px] border-[5px] rounded-[50%]" />
@@ -22,143 +28,38 @@ export default function Dashboard() {
           </div>
         </div>
 
-        <section className="bg-orange-400 rounded-xl flex items-end bg-[url(assets/images/icon-work.svg)] bg-no-repeat bg-position-[top_right_15px] min-h-[160px] xl:min-w-[100px] xl:min-h-[195px]">
-          <div className="flex-col justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[110px] xl:flex-col xl:min-h-[140px]">
-            <div className="flex justify-between">
-              <h2 className="font-medium">Work</h2>
-              <button type="button" className="min-h-[24px] hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg" aria-label="drop-down menu"><img src={btnIcon} /></button>
-            </div>
-            <div className="flex flex-row justify-between items-center xl:flex-col xl:items-start gap-1">
-              <p className="text-3xl xl:text-5xl">32hrs</p>
-              <p className="text-slate-300 pr-[5px]">Last Week - 36hrs</p>
-            </div>
+        {
+          boxes.map((item, index) => {
+            count++;
+            console.log(count);
+            return (
+              <section key={index} className={`${bgColor[count]} rounded-xl flex items-end ${bgImage[count]} bg-no-repeat bg-position-[top_right_15px] min-h-[160px] xl:min-w-[100px] xl:min-h-[195px]`}>
+                <div className="flex-col justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[110px] xl:flex-col xl:min-h-[140px]">
+                  <div className="flex justify-between">
+                    <h2 className="font-medium">{item.title}</h2>
+                    <button type="button" className="min-h-[24px] hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg" aria-label="drop-down menu"><img src={btnIcon} /></button>
+                  </div>
 
-            <span aria-hidden="true" hidden="true">
-              5hrs  daily
-              Previous - 7hrs  daily
-              32hrs  weekly
-              Previous - 36hrs  weekly
-              103hrs  monthly
-              Previous - 128hrs  monthly
-            </span>
-          </div>
-        </section>
+                  <div className="flex flex-row justify-between items-center xl:flex-col xl:items-start gap-1">
+                    <p className="text-3xl xl:text-5xl">{item.timeframes.weekly.current}hrs</p>
+                    <p className="text-slate-300 pr-[5px]">Last Week - {item.timeframes.weekly.previous}hrs</p>
+                  </div>
 
-        <section className="bg-blue-400 rounded-xl flex items-end bg-[url(assets/images/icon-play.svg)] bg-no-repeat bg-position-[top_right_15px] min-h-[160px] xl:min-w-[100px] xl:min-h-[195px]">
-          <div className="flex-col justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[110px] xl:flex-col xl:min-h-[140px]">
-            <div className="flex justify-between">
-              <h2 className="font-medium">Play</h2>
-              <button type="button" className="min-h-[24px] hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg" aria-label="drop-down menu"><img src={btnIcon} /></button>
-            </div>
-            <div className="flex flex-row justify-between items-center xl:flex-col xl:items-start gap-1">
-              <p className="text-3xl xl:text-5xl">10hrs</p>
-              <p className="text-slate-300 pr-[5px]">Last Week - 8hrs</p>
-            </div>
+                  <span aria-hidden="true" hidden={true}>
+                    5hrs  daily
+                    Previous - 7hrs  daily
+                    32hrs  weekly
+                    Previous - 36hrs  weekly
+                    103hrs  monthly
+                    Previous - 128hrs  monthly
+                  </span>
+                </div>
+              </section>
+            )
+          })
+        }
 
-            <span aria-hidden="true" hidden="true">
-              1hr  daily
-              Previous - 2hrs  daily
-              10hrs  weekly
-              Previous - 8hrs  weekly
-              23hrs  monthly
-              Previous - 29hrs  monthly
-            </span>
-          </div>
-        </section>
-
-        <section className="bg-red-400 rounded-xl flex items-end bg-[url(assets/images/icon-study.svg)] bg-no-repeat bg-position-[top_right_15px] min-h-[160px] xl:min-w-[100px] xl:min-h-[195px]">
-          <div className="flex-col justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[110px] xl:flex-col xl:min-h-[140px]">
-            <div className="flex justify-between">
-              <h2 className="font-medium">Study</h2>
-              <button type="button" className="min-h-[24px] hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg" aria-label="drop-down menu"><img src={btnIcon} /></button>
-            </div>
-            <div className="flex flex-row justify-between items-center xl:flex-col xl:items-start gap-1">
-              <p className="text-3xl xl:text-5xl">4hrs</p>
-              <p className="text-slate-300 pr-[5px]">Last Week - 7hrs</p>
-            </div>
-
-            <span aria-hidden="true" hidden="true">
-              0hrs  daily
-              Previous - 1hr  daily
-              4hrs  weekly
-              Previous - 7hrs  weekly
-              13hrs  monthly
-              Previous - 19hrs  monthly
-            </span>
-          </div>
-        </section>
-
-        <section className="bg-green-400 rounded-xl flex items-end bg-[url(assets/images/icon-exercise.svg)] bg-no-repeat bg-position-[top_right_15px] min-h-[160px] xl:min-w-[100px] xl:min-h-[195px]">
-          <div className="flex-col justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[110px] xl:flex-col xl:min-h-[140px]">
-            <div className="flex justify-between">
-              <h2 className="font-medium">Exercise</h2>
-              <button type="button" className="min-h-[24px] hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg" aria-label="drop-down menu"><img src={btnIcon} /></button>
-            </div>
-            <div className="flex flex-row justify-between items-center xl:flex-col xl:items-start gap-1">
-              <p className="text-3xl xl:text-5xl">4hrs</p>
-              <p className="text-slate-300 pr-[5px]">Last Week - 5hrs</p>
-            </div>
-
-            <span aria-hidden="true" hidden="true">
-              1hr  daily
-              Previous - 1hr  daily
-              4hrs  weekly
-              Previous - 5hrs  weekly
-              11hrs  monthly
-              Previous - 18hrs  monthly
-            </span>
-          </div>
-        </section>
-
-        <section className="bg-violet-600 rounded-xl flex items-end bg-[url(assets/images/icon-social.svg)] bg-no-repeat bg-position-[top_right_15px] min-h-[160px] xl:min-w-[100px] xl:min-h-[195px]">
-          <div className="flex-col justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[110px] xl:flex-col xl:min-h-[140px]">
-            <div className="flex justify-between">
-              <h2 className="font-medium">Social</h2>
-              <button type="button" className="min-h-[24px] hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg" aria-label="drop-down menu"><img src={btnIcon} /></button>
-            </div>
-            <div className="flex flex-row justify-between items-center xl:flex-col xl:items-start gap-1">
-              <p className="text-3xl xl:text-5xl">5hrs</p>
-              <p className="text-slate-300 pr-[5px]">Last Week - 10hrs</p>
-            </div>
-
-            <span aria-hidden="true" hidden="true">
-              1hr  daily
-              Previous - 3hrs  daily
-              5hrs  weekly
-              Previous - 10hrs  weekly
-              21hrs  monthly
-              Previous - 23hrs  monthly
-            </span>
-          </div>
-        </section>
-
-        <section className="bg-yellow-300 rounded-xl flex items-end bg-[url(assets/images/icon-self-care.svg)] bg-no-repeat bg-position-[top_right_15px] min-h-[160px] xl:min-w-[100px] xl:min-h-[195px]">
-          <div className="flex-col justify-between box bg-indigo-900 rounded-xl p-[20px] text-white w-full min-h-[110px] xl:flex-col xl:min-h-[140px]">
-            <div className="flex justify-between">
-              <h2 className="font-medium">Self Care</h2>
-              <button type="button" className="min-h-[24px] hover:bg-violet-500 active:bg-violet-800 p-[5px] rounded-lg" aria-label="drop-down menu"><img src={btnIcon} /></button>
-            </div>
-            <div className="flex flex-row justify-between items-center xl:flex-col xl:items-start gap-1">
-              <p className="text-3xl xl:text-5xl">2hrs</p>
-              <p className="text-slate-300 pr-[5px]">Last Week - 2hrs</p>
-            </div>
-
-            <span aria-hidden="true" hidden="true">
-              0hrs  daily
-              Previous - 1hr  daily
-              2hrs  weekly
-              Previous - 2hrs  weekly
-              7hrs  monthly
-              Previous - 11hrs  monthly
-            </span>
-          </div>
-        </section>
       </main>
-      
-      <footer className="attribution relative bottom-0 text-center -m-5 mt-5 text-white p-[20px] w-[355px] xl:absolute">
-        <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge"><span className="font-medium text-blue-200">Frontend Mentor</span></a></p>
-        <p>Coded by <a href="#"><span className="font-medium text-blue-200">Joshua Martinez</span></a></p>
-      </footer>
     </div>
-  );
+  )
 }
